0023afafe92c2550f97ec3301775ff9b2629becb
Arthur Neuman <arthurbneuman@gmail.com>
2022-11-21 00:18:10 -0500

    Switch from CRA to Vite

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import './client/styles/index.css'
+import './styles/index.css'
 
-import Main from './client/Main.jsx'
+import Main from './Main.jsx'
 
 ReactDOM.render(<Main/>, document.getElementById('root'))
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer()
+      ]
+    }
+  }
+})
